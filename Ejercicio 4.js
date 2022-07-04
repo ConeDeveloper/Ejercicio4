6b4fb685c4fd048b1e01adc1a5e2b299063e7ccf
ConeDeveloper <107569009+ConeDeveloper@users.noreply.github.com>
2022-07-04 11:57:41 -0300

    Add files via upload

---
+++ Ejercicio 4.js
@@ -0,0 +1,19 @@
+let Nombre = "Emiliano";
+let Apellido = "Perez";
+
+let Estudiante = Nombre + " " + Apellido;
+console.log(Estudiante)
+
+let estudianteMayus = Estudiante.toUpperCase
+console.log(Estudiante.toUpperCase())
+
+let estudianteMinus = Estudiante.tolowerCase
+console.log(Estudiante.toLowerCase())
+
+console.log(Estudiante.length)
+console.log(Nombre.charAt(0))
+console.log(Apellido.charAt(4))
+console.log(Estudiante.trim().length)
+console.log(Estudiante.includes(Nombre))
+
+
